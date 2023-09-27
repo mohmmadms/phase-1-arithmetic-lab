f6aeffdc15616c12b445d08717c027f8a9be03c0
mohmmad Smadi <mohmmadmms20@gmail.com>
2023-09-27 22:26:45 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+let num1=31;
+let num2=2;
+let multiply =num1 * num2;
+
+let random
+random =Math.floor((Math.random()*1000)+1);
+let num3,num4
+num3=14;
+num4=5;
+let mod =num3%num4;
+let max=Math.max(2,3,4,5,1,20)
+
+
+
 
 // Write your code here
